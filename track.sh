290d6d73f349630c242241926853fd55dd453e1d
Quentin JEROME <qjerome@rawsec.lu>
2025-05-21 10:12:23 +0200

    fix: track.sh

--- track.sh
+++ track.sh
@@ -11,12 +11,12 @@ else
     track -u -d
 fi
 
+git config --local user.name "Commit Bot"
+git config --local user.email "<>"
+    
 # if there are changes
 if [[ $(git status --short | wc -l) > 0 ]]
 then
-    git config --local user.name "Commit Bot"
-    git config --local user.email "<>"
-
     VERSION="v$(track -pv)"
     git add -A
     git commit -m "$VERSION"
